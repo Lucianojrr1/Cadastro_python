057d55b1f93d7e0c5a9527fb8cbbbbde57032a44
lucianojrr1 <lucianomjr14@gmail.com>
2025-01-14 13:34:22 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("Cadastro de Clientes")
+
+nome = st.text_input("Digite o nome do Cliente")
+endereco = st.text_input("Digite o endereco")
+dt_nasc = st.date_input("Escolha a data de Nascimento")
+tipo_cliente = st.selectbox("Tipo de Cliente", ["Pessoa Física","Pessoa Jurídica"])
+cadastrar = st.button("Cadastrar cliente")
+if cadastrar:
+    with open("clientes.csv", "a", encoding="utf8") as arquivo:
+        arquivo.write(f"{nome},{endereco},{dt_nasc},{tipo_cliente}\n")
+        st.success("Cliente cadastrado com sucesso.")
