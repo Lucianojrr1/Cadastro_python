621e368001d39b4eb71f868f15c62557f2dbdeaa
lucianojrr1 <lucianomjr14@gmail.com>
2025-01-14 13:50:25 -0300

    12

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title("Cadastro de Clientes")
 
 nome = st.text_input("Digite o nome do Cliente")
-endereco = st.text_input("Digite o endereco")
+endereco = st.text_input("Digite o endereco correto")
 dt_nasc = st.date_input("Escolha a data de Nascimento")
 tipo_cliente = st.selectbox("Tipo de Cliente", ["Pessoa Física","Pessoa Jurídica"])
 cadastrar = st.button("Cadastrar cliente")
